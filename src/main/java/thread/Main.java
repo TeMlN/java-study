8b69d3fd6afd1664c634aca10c6abb98d1cdbb26
taem1n1 <youman7953@naver.com>
2022-03-16 14:04:08 +0900

    Multi Thread

--- src/main/java/thread/Main.java
+++ src/main/java/thread/Main.java
@@ -1,2 +1,19 @@
-package thread;public class Main {
+package thread;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Thread thread0 = new Thread(new ThreadWithRunnable());
+        thread0.start();
+        System.out.println("thread0.getThreadGroup() = " + thread0.getThreadGroup()); //기본으로 생성된 ThreadGroup은 최대 우선순위가 10, 이름은 main으로 자동 설정된다.
+
+        ThreadGroup group = new ThreadGroup("myThread");
+        group.setMaxPriority(7);
+
+        Thread thread1 = new Thread(group, new ThreadWithRunnable());
+        thread1.start();
+        System.out.println("thread1.getThreadGroup() = " + thread1.getThreadGroup());
+
+    }
 }
+
--- src/main/java/thread/ThreadWithRunnable.java
+++ src/main/java/thread/ThreadWithRunnable.java
@@ -1,2 +1,11 @@
-package thread;public class ThreadWithRunnable {
+package thread;
+
+public class ThreadWithRunnable implements Runnable{
+    public void run() {
+        try {
+            Thread.sleep(10);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
 }
