b5d842270c5d0286558e75c0e0d11d01dca533d9
taem1n1 <youman7953@naver.com>
2022-04-14 20:30:22 +0900

    Thread를 상속하여 만드는 방법

--- src/main/java/thread/create_thread/CreateThreadByThread.java
+++ src/main/java/thread/create_thread/CreateThreadByThread.java
@@ -1,10 +1,13 @@
-package thread;
+package thread.create_thread;
 
 /**
- * Thrad를 상속해서 Thread를 생성하는 방법
+ * Thread를 상속해서 Thread를 생성하는 방법
+ *
+ * Thread를 상속해서 써야하는 상황 Override 즉 재정의 할게 많다면 Thread를 이용하여야 한다
+ * run, start, interrupted등 거의 모든 메소드들을 Override할 수 있다.
  */
 public class CreateThreadByThread extends Thread {
-    
+
     public void run() {
         System.out.println(Thread.currentThread().getName());
     }
