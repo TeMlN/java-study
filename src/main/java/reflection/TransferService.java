5b1a75d3e77e1c30146ebc91e905e6ae0ec7a039
taem1n1 <youman7953@naver.com>
2022-04-25 16:10:33 +0900

    Reflection을 공부하기 위해 만든 임의의 객체

--- src/main/java/reflection/TransferService.java
+++ src/main/java/reflection/TransferService.java
@@ -1,2 +1,13 @@
-package reflection;public class TransferService {
+package reflection;
+
+public class TransferService {
+    private int amount;
+
+    public void transfer(int amount) {
+        System.out.println("martial님이" + amount + "원 만큼 송금하였습니다.");
+    }
+
+    private void privateMethod() {
+        System.out.println("이 메소드는 자기자신 클래스에서만 접근이 가능해요!");
+    }
 }
