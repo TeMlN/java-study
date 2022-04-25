003bbe8d44b0015f5f08b38163a222b7a48e21b2
taem1n1 <youman7953@naver.com>
2022-04-25 16:09:50 +0900

    Finalizer 공격을 막는방법

--- src/main/java/finalizer/Account.java
+++ src/main/java/finalizer/Account.java
@@ -14,4 +14,9 @@ public class Account {
     public void transfer(int amount, String receiver) {
         System.out.printf("transfer %d from %s to %s", amount, this.name, receiver);
     }
+
+    //Finalizer 공격을 무효화 시키는 방법 final 키워드를 통한 상속 불가능
+    @Override
+    protected final void finalize() throws Throwable {
+    }
 }
