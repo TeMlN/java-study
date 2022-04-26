dc3fb27854489285683d352bdc61d2f996021320
taem1n1 <youman7953@naver.com>
2022-04-26 20:53:27 +0900

    직렬화 예제를 위한 클래스

--- src/main/java/serializable/Member.java
+++ src/main/java/serializable/Member.java
@@ -1,2 +1,25 @@
-package serializable;public class Member {
+package serializable;
+
+import java.io.Serializable;
+
+public class Member implements Serializable {
+
+    private String name;
+    private String email;
+    private int age;
+
+    public Member(String name, String email, int age) {
+        this.name = name;
+        this.email = email;
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Member{" +
+                "name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                ", age=" + age +
+                '}';
+    }
 }
