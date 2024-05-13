e3a7253c41d4b9e90235c03063f7d9d019bbada4
taem1n1 <youman7953@naver.com>
2024-05-13 22:34:44 +0900

    reflection 심화

--- src/main/java/annotation/Main.java
+++ src/main/java/annotation/Main.java
@@ -1,11 +1,14 @@
 package annotation;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ClassNotFoundException, NoSuchMethodException, InvocationTargetException, InstantiationException, IllegalAccessException, NoSuchFieldException {
         Arrays.stream(Service.class.getAnnotations()).forEach(System.out::println);
         Arrays.stream(ChildService.class.getAnnotations()).forEach(System.out::println);
 
@@ -23,5 +26,36 @@ public class Main {
                 }
         );
 
+
+        /**
+         * Reflection을 이용해 인스턴스를 만들때 권장되는 방법
+         */
+        Class<?> service = Class.forName("annotation.Service");
+
+        Constructor<?> deafultConstructor = service.getConstructor();
+        Service firstServiceInstance = (Service) deafultConstructor.newInstance();
+
+        Constructor<?> argsConstructor = service.getConstructor(String.class);
+        Service secondServiceInstance = (Service) argsConstructor.newInstance("a modify variable");
+
+        /**
+         * 생성한 인스턴스 사용
+         */
+        firstServiceInstance.printB();
+        secondServiceInstance.printB();
+
+
+        /**
+         * Reflection을 이용해 필드 조회
+         */
+
+        Field firstDeclaredField = Service.class.getDeclaredField("VARIABLE_A"); // 파라미터에 담은 값의 변수가 존재하지 않는다면 NoSuchFieldException 발생
+        System.out.println("before modify = " + firstDeclaredField.get(null));
+        firstDeclaredField.set(null,  9999999);
+        System.out.println("after modify = " + firstDeclaredField.get(null));
+
+        Field secondDeclaredField = Service.class.getDeclaredField("variableB");
+        secondDeclaredField.setAccessible(true);
+        System.out.println("secondDeclaredField = " + secondDeclaredField.get(firstServiceInstance));
     }
 }
--- src/main/java/annotation/Service.java
+++ src/main/java/annotation/Service.java
@@ -1,7 +1,26 @@
 package annotation;
 
+import jdk.dynalink.beans.StaticClass;
+
 @CustomAnnotation(dialect = Dialect.REDIS, ttl = 25600)
 public class Service {
 
+    public static Integer VARIABLE_A = 123;
 
+    private String variableB = "asd";
+
+    public Service() {
+    }
+
+    public Service(String variableB) {
+        this.variableB = variableB;
+    }
+
+    public void printB() {
+        System.out.println(variableB);
+    }
+
+    public void sumPrint(String first, String second) {
+        System.out.println(first + second);
+    }
 }
